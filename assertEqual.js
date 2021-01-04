93754829da32cb2517fd38dabfe2d8a843d56fc7
alexhauka <alexhauka@gmail.com>
2021-01-04 22:55:53 +0000

    assert function added

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("String", "string");
+assertEqual("string", "string");
+assertEqual(42, "42");
+assertEqual(42, 42);
