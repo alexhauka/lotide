7be94ac6c85ae2754fb2ff1909b3e917fbb8bcdd
alexhauka <alexhauka@gmail.com>
2021-01-05 22:44:36 +0000

    committing equal arrays function

---
+++ eqArrays.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  isEqual = true;
+  if (array1.length !== array2.length) {
+    isEqual = false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        isEqual = false;
+      }
+    }
+  }
+  return isEqual;
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
+assertEqual(eqArrays([], []), true)
+assertEqual(eqArrays(["1, 2, 3"], [1, 2, 3]), false)
