197c2e4c6c9de9a8a1c2fbae40f5d8b7c3ec071f
alexhauka <alexhauka@gmail.com>
2021-01-06 20:22:11 +0000

    added letter counting function (ignores spaces)

---
+++ countLetters.js
@@ -0,0 +1,15 @@
+const countLetters = function(sentance) {
+  let finalCount = {};
+  for (let letter of sentance) {
+    if (letter !== " ") {
+      if (finalCount[letter]) {
+        finalCount[letter] += 1;
+      } else {
+        finalCount[letter] = 1;
+      }
+    }
+  }
+  return finalCount;
+};
+
+console.log(countLetters('lighthouse labs'));
