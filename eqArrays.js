2d5e4145de26f882299cca6b427921115a2e4095
alexhauka <alexhauka@gmail.com>
2021-01-10 22:40:11 +0000

    refactored eqArrays to take in infinite nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -7,19 +7,32 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(array1, array2) {
-  let isEqual = true;
+
   if (array1.length !== array2.length) {
-    isEqual = false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        isEqual = false;
+    console.log(`${array1}'s length doesn't match ${array2}`)
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (Array.isArray(array1[i])) {
+      const isEqual = eqArrays(array1[i], array2[i])
+      if (!isEqual) {
+        console.log(`${array1[i]} does not equal ${array2[i]}`)
+        return false;
       }
+    } else if (array1[i] !== array2[i]) {
+      console.log(`hi`)
+    return false;
     }
   }
-  return isEqual;
+  return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([], []), true);
-assertEqual(eqArrays(["1, 2, 3"], [1, 2, 3]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true) // => true
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false) // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false) // => false
+
+
+// -------ORIGINAL CHECKS ------------------
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertEqual(eqArrays([], []), true);
+// assertEqual(eqArrays(["1, 2, 3"], [1, 2, 3]), false);
