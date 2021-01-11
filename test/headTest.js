d923a3db11bd4c377f262ef66f782a920c8a9558
alexhauka <alexhauka@gmail.com>
2021-01-11 18:28:27 +0000

    formatted tail test and head test to use mocha chai

--- test/headTest.js
+++ test/headTest.js
@@ -5,15 +5,15 @@ const assert = require("chai").assert;
 
 describe("head", () => {
   it("should return 1 for [1, 2, 3]", () => {
-    assert.strictEqual(head([1, 2, 3]), 1);
+    assert.deepEqual(head([1, 2, 3]), 1);
   });
 
   it("should return '5' for ['5']", () => {
-    assert.strictEqual(head(['5']), '5');
+    assert.deepEqual(head(['5']), '5');
   });
 
   it("should return undefined for '[]'", () => {
-    assert.strictEqual(head([]), undefined);
+    assert.deepEqual(head([]), undefined);
   });
 
 });
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,27 @@
 const tail = require("../tail");
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+describe("#tail", () => {
+
+  it("should return ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse, 'Labs']", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    assert.deepEqual(tail(words), ['Lighthouse', 'Labs']);
+  });
+
+  it("should leave the original array intact", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    const result = tail(words);
+    assert.deepEqual(words, ["Yo Yo", "Lighthouse", "Labs"]);
+  });
+
+  it("should return an array with length 1 shorter than original (e.g. 3 -> 2)", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    const result = tail(words);
+    assert.deepEqual(result.length, 2);
+  });
+  it("should return 'Lighthouse' for element 0", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    const result = tail(words);
+    assert.deepEqual(result[0], 'Lighthouse');
+  });
+});
