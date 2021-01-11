9e1057faa85473e120f28084608de26862144120
alexhauka <alexhauka@gmail.com>
2021-01-11 16:24:13 +0000

    refactored assertArraysEqual and created test

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,4 @@
-const eqArrays = function(array1, array2) {
-  let isEqual = true;
-  if (array1.length !== array2.length) {
-    isEqual = false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        isEqual = false;
-      }
-    }
-  }
-  return isEqual;
-};
+const eqArrays = require("./eqArrays");
 
 
 const assertArraysEqual = function(actual, expected) {
@@ -21,5 +9,4 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([], []);
+module.exports = assertArraysEqual;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,7 @@
+const assertArraysEqual = require("../assertArraysEqual");
+// const eqArrays = require("../eqArrays");
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([], []);
+assertArraysEqual([1, 2, 3], ["1", 2, 3]);
+assertArraysEqual([3, 2, 1], [1, 2, 3]);
